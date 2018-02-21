15cb7347629973bfa8d553eee3f83117216c0822
Mauricio Alvim <mauricio@MacBook-Pro-de-Mauricio.local>
2018-02-20 23:11:46 -0300

    Initial commit.

---
+++ app/scripts.babel/background.js
@@ -0,0 +1,11 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(details => {
+  console.log('previousVersion', details.previousVersion);
+});
+
+chrome.tabs.onUpdated.addListener(tabId => {
+  chrome.pageAction.show(tabId);
+});
+
+console.log('We r here!');
