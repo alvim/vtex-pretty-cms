1e3556708da5fc279021dcf6f5e7a2216fcd8629
Mauricio Alvim <malvimmacedo@gmail.com>
2018-03-03 11:15:04 -0300

    Delegate chrome events to update Popup.

--- src/Popup/index.vue
+++ src/Popup/index.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>{{ msg }}</div>
+  <div class="actions">
+    <div class="action" v-for="view in views">
+      <p>{{ view.name }}</p>
+      <p>{{ view.title }}</p>
+      <p>{{ view.description }}</p>
+    </div>
+    <div v-if="!views">Não há ações disponíveis</div>
+    <p>Lindo</p>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: 'Eita'
+      msg: 'Eita',
+      views: null
     }
+  },
+  created() {
+    chrome.tabs.query({ active: true }, ([{ id }]) => {
+      return chrome.tabs.sendMessage(id, { type: 'REQUEST_AVAILABLE_ACTIONS' })
+    })
+
+    chrome.runtime.onMessage.addListener(({ type, views }, sender, sendResponse) => {
+      switch(type) {
+        case 'UPDATE_VIEW':
+          this.views = views
+          break
+        default:
+          null
+      }
+    })
   }
 }
 </script>
--- src/popup_index.js
+++ src/popup_index.js
@@ -1,29 +1,10 @@
 import Vue from 'vue'
 import Popup from './Popup/index.vue'
 
-const requestState = tabId => chrome.tabs.sendMessage(tabId, { type: 'REQUEST_AVAILABLE_ACTIONS' })
-
-chrome.tabs.onUpdated.addListener(tabId => requestState(tabId))
-
-chrome.runtime.onMessage.addListener(({ type, views }, sender, sendResponse) => {
-  switch(type) {
-    case 'UPDATE_VIEW':
-      window.pp.views = views
-      break
-    default:
-      null
-  }
+let vm = new Vue({
+  el: '#actions-container',
+  render: h => h(Popup)
 })
 
-const mountPopup = () => {
-  new Vue({
-    el: '#actions-container',
-    render: h => h(Popup)
-  })
-}
 
-mountPopup()
 
-// NEXT STEP:
-// DEFAULT VIEW
-// UPDATE VIEW
