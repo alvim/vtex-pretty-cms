7dd1e38b9b7657d390d130c5bea98873a493a83a
Mauricio Alvim <malvimmacedo@gmail.com>
2018-03-04 20:16:01 -0300

    Popup initial css.

--- src/Popup/index.vue
+++ src/Popup/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="actions">
-    <button class="action" v-for="view in views" @click="dispatch(view.name)">
-      <p>{{ view.title }}</p>
-      <p>{{ view.description }}</p>
-    </button>
-    <div v-if="!views">Não há ações disponíveis</div>
-    <p>Lindo</p>
+    <div v-if="!views">No actions available.</div>
+    <div v-else>
+      <h2 class="title">Store name</h2>
+      <div class="actions-container">
+        <button class="action" v-for="view in views" @click="dispatch(view.name)" :title="view.description">
+          {{ view.title }}
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -42,3 +45,33 @@ export default {
   }
 }
 </script>
+
+<style>
+.action {
+  flex: 1 1;
+  flex-basis: calc(50% - 10px);
+  border-radius: 10px;
+  border: 0;
+  width: 100%;
+  text-align: center;
+  background: #F51963;
+  color: #fff;
+  font-weight: bold;
+  font-size: 12px;
+  margin: 3px;
+  padding: 10px;
+  outline: none;
+  cursor: pointer;
+}
+
+.title {
+  text-align: center;
+  font-size: 14px;
+}
+
+.actions-container {
+  display: flex;
+  flex-flow: row wrap;
+  align-items: stretch;
+}
+</style>
